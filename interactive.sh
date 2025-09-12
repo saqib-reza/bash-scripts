be1cc0dad38f494fc80a31e8aadb5e800e9f22e9
Saqib Reza <saqib.reza11@gmail.com>
2025-09-12 06:13:03 +0000

    Second commit

---
+++ interactive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Take user input
+
+echo "Hey what is your name "
+read name
+echo "Please enter your age "
+read age 
+echo "Today is $(date)"
+echo "Thanks Mr./Mrs. $name "
+
---
+++ process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Print current processes
+
+echo "Hello $USER "
+echo "Hey i am $USER and will be telling you about the current processes"
+echo "Running processes list "
+ps
+
