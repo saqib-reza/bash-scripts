d67310d3eb1411859d87931bc34392de770777ef
Saqib Reza <saqib.reza11@gmail.com>
2025-09-15 13:27:29 +0000

    Add factorial script

---
+++ factorial.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Print factorial of a number
+
+echo -n "Enter The Number: "
+read -r a
+fact=1
+while [ "$a" -ne 0 ]; do
+	fact=$((fact * a))
+	a=$((a - 1))
+done
+echo $fact
