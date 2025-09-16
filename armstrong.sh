6271b912bca55e491ff07ad0a53c669104aa0628
Saqib Reza <saqib.reza11@gmail.com>
2025-09-16 15:49:51 +0000

    Add armstrong.sh

---
+++ armstrong.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Check if number is armstrong or not
+
+echo -n "Enter A Number: "
+read -r n
+arm=0
+temp=$n
+while [ "$n" -ne 0 ]; do
+	r=$((n % 10))
+	arm=$((arm + r * r * r))
+	n=$((n / 10))
+done
+echo $arm
+if [ $arm -eq "$temp" ]; then
+	echo "Armstrong"
+else
+	echo "Not Armstrong"
+fi
